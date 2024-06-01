8447692cdc651afd0f08bdd360e27de64d20689b
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2024-06-01 16:03:52 -0300

    Starting Java

---
+++ Java_GS_Poseidon/src/main/java/com/poseidon/app/Application.java
@@ -0,0 +1,15 @@
+package com.poseidon.app;
+
+import java.util.Scanner;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.print("Ola mundo");
+		
+		sc.close();
+	}
+
+}

98c422f36c17af7e5c84f1ca4c2d2fd8ca3be7a2
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2024-06-01 22:05:55 -0300

    creating model and connection

---
+++ Java_GS_Poseidon/src/main/java/com/poseidon/model/ONG.java
@@ -0,0 +1,13 @@
+package com.poseidon.model;
+
+public class ONG {
+	
+	private Integer Id_ONG;
+	private String Nome;
+    private String Email;
+    private String Senha;
+    private String Endereco;
+    private String Telefone;
+    private String Descricao;
+
+}
---
+++ Java_GS_Poseidon/src/main/java/com/poseidon/model/Usuario.java
@@ -0,0 +1,12 @@
+package com.poseidon.model;
+
+public class Usuario {
+	
+	private Integer Id_Usuario;
+	private String Nome;
+	private String Email;
+	private String Senha;
+	private String Endereco;
+	private String Telefone;
+
+}
